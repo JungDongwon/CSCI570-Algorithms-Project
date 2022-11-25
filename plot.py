e1332881f76516b1bf85f091c95650fea03292e3
zyam <mshim1007@gmail.com>
2022-11-24 20:03:45 -0800

    automated extracting result values in plot.py

--- plot.py
+++ plot.py
@@ -46,11 +46,63 @@ ProblemSize = [16,
                3584,
                3968]
 
+import enum
+import os
+import natsort
 
+bas_file_list = []
+eff_file_list = []
+
+path="/Users/minsunshim/Python/570_project/CSCI570-Algorithms-Project/Results/"
+
+file_list = os.listdir(path+"outputFiles/")
+file_list = natsort.natsorted(file_list)
+
+for file in file_list:
+    if file.startswith('bas'):
+        bas_file_list.append(file)
+    else:
+        eff_file_list.append(file)
+
+eff_CPUTime = []
+eff_Memory = []
+eff_ProblemSize = []
+
+bas_CPUTime = []
+bas_Memory = []
+bas_ProblemSize = []
+
+for b in bas_file_list:
+    with open(path+"outputFiles/"+b, 'r') as file:
+        for i, line in enumerate(file):
+            if i == 3: #time
+                new = line.replace('\n', '')
+                bas_CPUTime.append(float(new))
+            elif i == 4: #memory
+                new = line.replace('\n', '')
+                bas_Memory.append(float(new))
+
+for e in eff_file_list:
+    with open(path+"outputFiles/"+e, 'r') as file:
+        for i, line in enumerate(file):
+            if i == 3: #time
+                new = line.replace('\n', '')
+                eff_CPUTime.append(float(new))
+            elif i == 4: #memory
+                new = line.replace('\n', '')
+                eff_Memory.append(float(new))
+
+# print(bas_CPUTime)
+# print(bas_Memory)
+
+# print(eff_CPUTime)
+# print(eff_Memory)
+
+#basic
 import matplotlib.pyplot as plt
 
 # plotting the points 
-plt.plot(CPUTime, ProblemSize)
+plt.plot(bas_CPUTime, ProblemSize)
   
 # naming the x axis
 plt.xlabel('CPU Time in milliseconds')
@@ -60,13 +112,16 @@ plt.ylabel('Problem Size')
 # giving a title to my graph
 plt.title('(Basic) CPU time vs problem size')
   
-# function to show the plot
-plt.show()
+# # function to show the plot
+# plt.show()
 
+#Save the plot
+plt.savefig(path+'(Basic)CPU_Time&Size.png')
+plt.clf() #clear the plot
 
 ######### Memory usage vs problem size
 # plotting the points 
-plt.plot(Memory, ProblemSize)
+plt.plot(bas_Memory, ProblemSize)
   
 # naming the x axis
 plt.xlabel('Memory usage in KB')
@@ -76,10 +131,51 @@ plt.ylabel('Problem Size')
 # giving a title to my graph
 plt.title('(Basic) Memory usage vs problem size')
   
-# function to show the plot
-plt.show()
+# # function to show the plot
+# plt.show()
+
+#Save the plot
+plt.savefig(path+'(Basic)Mem_Use&Size.png')
+plt.clf() #clear the plot
+
+############################# Efficient #############################
+
+# plotting the points 
+plt.plot(eff_CPUTime, ProblemSize)
+  
+# naming the x axis
+plt.xlabel('CPU Time in milliseconds')
+# naming the y axis
+plt.ylabel('Problem Size')
+  
+# giving a title to my graph
+plt.title('(Efficient) CPU time vs problem size')
+  
+# # function to show the plot
+# plt.show()
+
+#Save the plot
+plt.savefig(path+'(Efficient)CPU_Time&Size.png')
+plt.clf() #clear the plot
 
 
+######### Memory usage vs problem size
+# plotting the points 
+plt.plot(eff_Memory, ProblemSize)
+  
+# naming the x axis
+plt.xlabel('Memory usage in KB')
+# naming the y axis
+plt.ylabel('Problem Size')
+  
+# giving a title to my graph
+plt.title('(Efficient) Memory usage vs problem size')
+  
+# # function to show the plot
+# plt.show()
+
+#Save the plot
+plt.savefig(path+'(Efficient)Mem_Use&Size.png')
 
 # (1)
 # 0.087738037109375
